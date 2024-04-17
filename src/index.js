a9f63d86544f99dfaa2eec348ff55cb7e49246b0
Akesh-Kumar1 <akumar7_be21@thapar.edu>
2024-04-17 16:34:59 +0530

    Write Some Utility code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
+import {app} from "./app.js"
 dotenv.config({
     path: './env'
 })
@@ -7,7 +8,20 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>
+{
+  app.on("error",(error)=>
+{
+  console.log("Error is: ",error)
+})
+  app.listen(process.env.PORT||8000,()=>
+{
+  console.log(`Server running at port: ${process.env.PORT}`)
+})
+})
+.catch((err)=>{
+   console.log("MONGO db Connection failed !!!",err)
+})
 
 
 
