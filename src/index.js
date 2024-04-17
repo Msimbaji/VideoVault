c3496ab31bc7bb8bea7692df6bf32beeccccd688
Akesh-Kumar1 <akumar7_be21@thapar.edu>
2024-04-17 15:33:37 +0530

    Database Connected

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=>{
+  try {
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",()=>{
+        console.log("Error:",error)
+        throw error
+    })
+    app.listen(process.env.PORT,()=>
+{
+    console.log(`App is listening at ${process.env.PORT}`)
+})
+  } catch (error) {
+    console.error("ERROR:",error)
+    throw error
+    
+  }
+
+})()
+*/
